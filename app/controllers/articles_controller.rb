a0a55a22a4d149130725ebdb90f9c5182bcf6d6f
Sergio Oseko <osk.sergio@gmail.com>
2024-02-12 18:01:03 -0300

    feat: definindo os parâmetros permitidos no cadastro do article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -12,12 +12,18 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    @article = Article.new()
+    @article = Article.new(article_paramns)
 
     if @article.save
       redirect_to @article
     else
       render :new
     end
+  end
+
+  private
+
+  def article_paramns
+    params.require(:article).permit(:title, :body)
   end
 end
